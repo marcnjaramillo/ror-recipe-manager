426c329aa3b5b14298948dda051607f19518e896
marcnjaramillo <marcn.jaramillo@gmail.com>
2017-03-24 00:49:04 -0700

    Update routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
+
+  root 'recipes#index'
+
   resources :recipes
-  resources :users
+
+  resources :users, only: [:create, :show, :edit, :update, :destroy] do
+    resources :recipes
+  end
+
+  get 'signup', to: 'users#new'
+  get 'signin', to: 'sessions#new'
+  post 'signin', to: 'sessions#create'
+  get 'signout', to: 'sessions#destroy'
+
+  get '/auth/facebook/callback', to: 'sessions#auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
