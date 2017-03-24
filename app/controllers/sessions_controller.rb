707481545becd8709537e5f3d693ebd2fb963522
marcnjaramillo <marcn.jaramillo@gmail.com>
2017-03-24 00:14:35 -0700

    Write code for sessions actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(username: params[:session][:username])
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      redirect_back_or @user
+    else
+      flash.now[:danger] = "Invalid email/password combination"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:success] = "You have been signed out."
+    redirect_to root_url
+  end
 end
