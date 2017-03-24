5e8483b73fcc7ea4cf54671317ab72e23984946f
marcnjaramillo <marcn.jaramillo@gmail.com>
2017-03-24 00:00:58 -0700

    Write code for user and recipe actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,76 @@
 class RecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.search(params[:search])
+  end
+
+  def new
+    @recipe = Recipe.new
+    10.times {
+      @recipe.recipe_ingredients.build.build_ingredient
+    }
+    5.times {
+      @recipe.directions.build
+    }
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    if @recipe.save
+      flash[:success] = "Recipe successfully saved."
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+    10.times {
+      @recipe.recipe_ingredients.build.build_ingredient
+    }
+    5.times {
+      @recipe.directions.build
+    }
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = "Recipe successfully updated."
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:id])
+    user = recipe.creator
+    recipe.destroy
+    flash[:success] = "Recipe successfully deleted."
+    redirect_to user_path(user)
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :name,
+      :prep_time,
+      :cook_time,
+      :user_id,
+      :search,
+      directions_attributes: [:id, :text],
+      recipe_ingredients_attributes: [
+        :id,
+        :quantity,
+        ingredient_attributes: [:id, :name]
+      ]
+    )
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,49 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "Congratulations! You have successfully signed up."
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @recipes = @user.recipes
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your information successfully updated."
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    session.delete(:user_id)
+    flash[:success] = "Your account has been successfully deleted."
+    redirect_to root_url
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :username, :email, :password)
+  end
 end
