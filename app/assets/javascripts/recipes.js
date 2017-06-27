f80d88124a357afc8a9ff5f737b7cb6d5adb595e
marcnjaramillo <marcn.jaramillo@gmail.com>
2017-06-27 09:18:00 -0700

    Rename recipes.coffee to recipes.js and write code for application

---
+++ app/assets/javascripts/recipes.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("#new_ingredient_button").click(function(event) {
+    var $button = $(this);
+    var url = $(this).data("url")
+
+    $.get(url, function(response) {
+      $button.before(response)
+    })
+    event.preventDefault();
+      addNewIngredient();
+  });
+});
+
+var addNewIngredient = function() {
+   var $new_ingredients = $("#new_ingredients");
+   var new_index = $("#new_ingredients input").length / 2;
+   var ingredientInput = `<name="recipe[ingredients_attributes][${new_index}][name]" id="recipe_ingredients_attributes_${new_index}_name"> `;
+   ingredientInput += `< name="recipe[ingredients_attributes][${new_index}][recipe_ingredients_attributes][0][quantity]" id="recipe_ingredients_attributes_${new_index}_recipe_ingredients_attributes_0_quantity">`
+   $new_ingredients.append(ingredientInput);
+}
