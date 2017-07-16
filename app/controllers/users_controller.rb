342f7be8a07aa6b70901f1564de1cb0017d76105
marcnjaramillo <marcn.jaramillo@gmail.com>
2017-07-16 01:06:40 -0700

    Create general landing page for site

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
 
+  def home
+
+  end
+
   def new
     @user = User.new
   end
