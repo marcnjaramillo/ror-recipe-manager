9a3d75d736b2af582a28735150ee4abc9d112377
marcnjaramillo <marcn.jaramillo@gmail.com>
2017-09-09 13:58:52 -0700

    Create new migration for comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324043606) do
+ActiveRecord::Schema.define(version: 20170908184450) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "recipe_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "directions", force: :cascade do |t|
     t.integer  "recipe_id"
