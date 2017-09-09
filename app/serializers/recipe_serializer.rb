ed83eda47f3171b8659307c863503a8fd8c923a9
marcnjaramillo <marcn.jaramillo@gmail.com>
2017-09-09 14:06:31 -0700

    Update recipe serializer

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,7 +1,18 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :name, :prep_time, :cook_time
+  attributes :id, :name, :prep_time, :cook_time, :comment_list
   has_many :recipe_ingredients
   has_many :directions
 
-
+  def comment_list
+    object.comments.map do |comment|
+      {
+        id: comment.id,
+        user: {
+          id: comment.user_id,
+          username: User.find(comment.user_id).username
+        },
+        content: comment.content
+      }
+    end
+  end
 end
