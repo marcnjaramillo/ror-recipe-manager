ce58269468a52b65e24a2f616e71e06deaf2d3a6
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-07-13 09:17:49 -0700

    Clean up recipe model, remove ingredients_attributes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,28 +1,23 @@
 class Recipe < ApplicationRecord
+  belongs_to :user
+
+  has_many :comments, dependent: :destroy
+  has_many :directions
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
-  has_many :directions
-  has_many :comments
-  belongs_to :user, foreign_key: "user_id"
 
   validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :recipe_ingredients, presence: true
+  validates :directions, presence: true
 
-  accepts_nested_attributes_for :recipe_ingredients, :reject_if => proc { |att| att['name'].blank? && att['quantity'].blank? }
-  accepts_nested_attributes_for :directions, :reject_if => proc { |att| att['text'].blank? }
+  accepts_nested_attributes_for :recipe_ingredients, reject_if: proc { |attr| attr[:quantity].blank? && attr[:ingredient_attributes][:name].blank? }, allow_destroy: true
+  accepts_nested_attributes_for :directions, reject_if: proc { |attr| attr[:text].blank? }, allow_destroy: true
 
   scope :sorted, lambda {order("created_at DESC")}
   scope :search, lambda {|search| where(["name LIKE ?", "%#{search}%"])}
 
-  def ingredients_attributes=(ingredient_attributes)
-    ingredient_attributes.each do |ingredient_hash|
-      if ingredient_hash[:name] != ""
-        ingredient = Ingredient.find_or_create_by(name: ingredient_hash[:name])
-        self.recipe_ingredients.build(ingredient: ingredient, quantity: ingredient_hash[:quantity])
-      end
-    end
-  end
-
   def empty_children(attributes)
-    attributes['name'].blank? && attributes['quantity'].blank?
+    attributes[:name].blank? && attributes[:quantity].blank?
   end
 end
