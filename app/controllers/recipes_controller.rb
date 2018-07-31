9a5c820234b7b1ce81810bfa584dcd4c47612adb
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-07-30 20:10:40 -0700

    Update files - still debugging

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,5 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: [:show,:edit,:update,:destroy]
-  before_action :set_s3_direct_post, only: [:new, :edit, :create, :update]
 
   def index
     @recipes = Recipe.search(params[:search])
@@ -82,9 +81,5 @@ class RecipesController < ApplicationController
       :id,
       :quantity,
       ingredient_attributes: [:id, :name]])
-  end
-
-  def set_s3_direct_post
-    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
   end
 end
