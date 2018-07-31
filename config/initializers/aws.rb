c19a0b8e7aef18051c0e2e59a1224d4a43650971
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-07-30 19:04:14 -0700

    Further attempt to fix deployment issue

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,8 +1,6 @@
 require 'aws-sdk-s3'
 
 Aws.config.update({
+  bucket: 'my-recipe-box',
   region: 'us-west-2',
-  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
 })
-
-S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['BUCKET_NAME'])
